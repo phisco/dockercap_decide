ca343f2359abf339a64a52d28d42ca9029562962
Philippe scorsolini <pscorso93@gmail.com>
2017-12-01 17:27:18 +0100

    moved to stdout instead of .json file

--- test/rapl_sweep.py
+++ test/rapl_sweep.py
@@ -1,24 +1,18 @@
 import os
-import json
 from parse import *
 from time import *
+from pprint import *
 
 
 def test(benchmark, minW, maxW, step):
     output=[]
     for i in range(minW, maxW, step):
         # set rapl
-
         output.append({'date': time(), 'rapl': {}, 'perf': run_perf_return_dict(benchmark)})
-    with open('data_{}_{}W_{}W_{}.json'.format(benchmark,minW,maxW,time()),'w+') as file:
-        json.dump(output, file)
-
-
-
+    return output
 
 def run_perf_return_dict(benchmark):
     output = os.popen('perf stat {} 2>&1 1>/dev/null'.format(benchmark)).read()
-    print output
     output = output.split('\n') #split on \n ['val key # val key'.]
     output = map(lambda x: x.split('#'), output) #split on # : ['value key # value key',] -> [['value key', 'value key'],]
     output = [filter(lambda x: x is not '', el) for el in output] #remove empty elements
@@ -37,14 +31,9 @@ def run_perf_return_dict(benchmark):
             o[line[0][1]] = {'value': line[0][0], line[1][1]: line[1][0]}
         elif len(line) is 1:
             o[output[old_n][0][1]][line[0][1]] = line[0][0]
-    print o
     return o
 
 
 if __name__ == "__main__":
-    run_perf_return_dict('ls')
-
-
-
-
+    pprint(run_perf_return_dict('ls'))
 
