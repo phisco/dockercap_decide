437199d9c177ba82ad5095c932805f8941163c90
Philippe scorsolini <pscorso93@gmail.com>
2017-12-06 22:15:00 +0100

    output to stdout

--- test/master/master.py
+++ test/master/master.py
@@ -2,14 +2,16 @@ import pika
 from time import sleep,time
 import docker
 import os
+import logging
 
 LOCAL_PATH = '/home/users/marco.arnaboldi/hyppo/powercap/_build/utils'
 NUM_PACKAGES = 2
 NUM_WORKERS = 2
-TIME = 10
+TIME = 1
+logging.basicConfig(level=logging.INFO)
 
 def callback(ch, method, properties, body):
-    print(time())
+    print(body)
 
 def test(minW=50, maxW=200, step=10):
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672))
@@ -20,10 +22,13 @@ def test(minW=50, maxW=200, step=10):
     queue_name = result.method.queue
     channel.queue_bind(exchange='responses', queue=queue_name)
 
-    cmd_channel = connection.channel()
+    cmd_connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672))
+    cmd_channel = cmd_connection.channel()
     for i in range(maxW, minW, -step):
+        print(i)
         for p in range(NUM_PACKAGES):
             os.system('{0}/rapl-set --package={1} --constraint=0 --c-power-limit={2}'.format(LOCAL_PATH, p, i*1000000))
+
         cmd_channel.basic_publish(exchange='command', routing_key='', body='execute')
         sleep(TIME)
     cmd_channel.close()
--- test/slave/slave.py
+++ test/slave/slave.py
@@ -1,4 +1,7 @@
 import pika
+import logging
+from time import time
+import os
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', port=5672))
 channel = connection.channel()
@@ -6,18 +9,18 @@ channel.exchange_declare(exchange='command', exchange_type='fanout')
 channel.exchange_declare(exchange='responses', exchange_type='fanout')
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
-channel.queue_bind(exchange='command' , queue=queue_name)
-print(' [*] Waiting for commands. To exit press CTRL+C')
+channel.queue_bind(exchange='command', queue=queue_name)
+resp_connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit', port=5672))
+logging.basicConfig(level=logging.INFO)
 
-
+resp_channel = resp_connection.channel()
 def callback(ch, method, properties, body):
-    #doStuff
-    message = 'HELLO WORLD'
-    resp_channel = connection.channel()
-    resp_channel.basic_publish(exchange='responses', routing_key='', body=message)
-    resp_channel.close()
+    os.popen('/home/users/marco.arnaboldi/hyppo/stress-ng-0.09.04/stress-ng --cpu 40 --udp 100 --memcpy 100 -t 10 2>&1 1>/dev/null')
+    os.logging.info(body)
+    resp_channel.basic_publish(exchange='responses', routing_key='', body=str(time()))
 
 
 channel.basic_consume(callback, queue=queue_name, no_ack=True)
+logging.info(' [*] Waiting for commands. To exit press CTRL+C')
 
 channel.start_consuming()
